c8b5dc50bb52c0af0d85324f7c8985118fdcff25
ne17413024 <69644368+ne17413024@users.noreply.github.com>
2021-10-15 18:04:44 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var fedTime,lastFed;
+var feed,addFood;
+var foodObj;
+
+function preload(){
+sadDog=loadImage("Images/Dog.png");
+happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+  
+  feed=createButton("Alimenta al perro");
+  feed.position(680,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Agregar Alimento");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+ 
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Última hora en que se alimentó : "+ lastFed%12 + " PM", 350,60);
+   }else if(lastFed==0){
+     text("Última hora en que se alimentó : 12 AM",350,60);
+   }else{
+     text("Última hora en que se alimentó : "+ lastFed + " AM", 350,60);
+   }
+ 
+  drawSprites();
+}
+
+//función para leer la Existencia de alimento
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+//función para actualizar la existencia de alimento, y la última hora en la que se alimentó
+function feedDog(){
+  dog.addImage(happyDog);
+  
+  if(foodObj.getFoodStock()<= 0){
+    foodObj.updateFoodStock(foodObj.getFoodStock()*0);
+  }else{
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  }
+  
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+  })
+}
+
+//función para agregar el alimento al almacén
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
